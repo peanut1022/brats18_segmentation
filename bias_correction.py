755c97893b4a9c4041ca5c270ca2378861c9060c
peanut1022 <37501283+peanut1022@users.noreply.github.com>
2018-11-13 22:14:53 +0800

    Update bias_correction.py

--- bias_correction.py
+++ bias_correction.py
@@ -3,11 +3,10 @@ import os, sys, glob
 import numpy as np
 import nibabel as nib
 from nipype.interfaces.ants.segmentation import N4BiasFieldCorrection
-#from SimpleITK import N4BiasFieldCorrection
 from skimage.transform import resize
 from multiprocessing import Pool, cpu_count
 
-#利用n4做偏置场校正
+#using n4 for bias_correction
 def n4_correction(im_input):
     n4 = N4BiasFieldCorrection()
     n4.inputs.dimension = 3
@@ -19,12 +18,12 @@ def n4_correction(im_input):
     n4.run()
 
 def batch_works(k):
-    input_path = "G:/brats17-master/Brats17/Brats17TrainingData"
+    input_path = "G:/brats18-master/Brats18/Brats18TrainingData"
     all_paths = []
     for dirpath, dirnames, files in os.walk(input_path):
-        if os.path.basename(dirpath)[0:7] == 'Brats17':
+        if os.path.basename(dirpath)[0:7] == 'Brats18':
             all_paths.append(dirpath)            
-    n_processes = 8
+    n_processes = cpu_count()
       
     if k == n_processes - 1:
         paths = all_paths[k * int(len(all_paths) / n_processes) : ]
@@ -35,20 +34,7 @@ def batch_works(k):
         n4_correction(glob.glob(os.path.join(path, '*_t1.nii.gz'))[0])
         n4_correction(glob.glob(os.path.join(path, '*_t1ce.nii.gz'))[0])
     
-if __name__ == '__main__':
-# =============================================================================
-#     if len(sys.argv) < 2:
-#         raise Exception("Need at least the input data directory")
-#     input_path = sys.argv[1]
-#     
-#     input_path = "G:/brats17-master"
-#     all_paths = []
-#     for dirpath, dirnames, files in os.walk(input_path):
-#         if os.path.basename(dirpath)[0:9] == 'Brats17':
-#             all_paths.append(dirpath)
-#    n_processes = cpu_count()
-# =============================================================================
-    
-    n_processes = 8    
+if __name__ == '__main__':  
+    n_processes = cpu_count()    
     pool = Pool(processes=n_processes)
-    pool.map(batch_works(5), range(n_processes))
+    pool.map(batch_works, range(n_processes))
